aff7720a583c5ec1b1d203a235c6a6b1dc1d9330
wmoooid <threeqletters@yandex.ru>
2022-02-20 16:57:21 +0400

    small fix

--- components/Header/Userbar/Userbar.tsx
+++ components/Header/Userbar/Userbar.tsx
@@ -7,21 +7,33 @@ export const Userbar: React.FC = () => {
   const { data, isLoading, isError } = useUser();
 
   if (isLoading) {
-    return <UserbarPlaceholder />;
+    return (
+      <a
+        href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT_URL}&duration=permanent&scope=read submit identity`}>
+        <UserbarPlaceholder />
+      </a>
+    );
   }
 
   if (isError) {
-    return <UserbarPlaceholder />;
+    return (
+      <a
+        href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT_URL}&duration=permanent&scope=read submit identity`}>
+        <UserbarPlaceholder />
+      </a>
+    );
   }
 
   if (data) {
     return (
-      <>
+      <a
+        className={styles.profile}
+        href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT_URL}&duration=permanent&scope=read submit identity`}>
         <span className={styles.userAvatar}>
           <img className={styles.userAvatarImg} src={data.icon_img} alt='User avatar' />
         </span>
         <span className={styles.userName}>{data.name}</span>
-      </>
+      </a>
     );
   }
 
