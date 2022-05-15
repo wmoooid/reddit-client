811faaec285bfe51a11b9d863597357a166d1993
wmoooid <threeqletters@gmail.com>
2022-05-15 17:53:06 +0400

    `useStoriesList` refactor

--- components/PostList/StoriesBar/StoriesBar.tsx
+++ components/PostList/StoriesBar/StoriesBar.tsx
@@ -1,15 +1,16 @@
+import useStoriesList from '@/hooks/useStoriesList';
 import useSubscriptions from '@/hooks/useSubscriptions';
 import Link from 'next/link';
 import React from 'react';
 import styles from './StoriesBar.module.css';
 
 export const StoriesBar: React.FC = () => {
-  const { subscriptions } = useSubscriptions();
+  const { data } = useStoriesList();
 
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
-        {subscriptions.map((item) => (
+        {data.map((item) => (
           <Link key={item.data.id} href={`/stories${item.data.url}`} shallow={true}>
             <li className={styles.item}>
               <img className={styles.itemImage} src={item.data.community_icon || item.data.icon_img} alt='' />
--- hooks/useStoriesList.tsx
+++ hooks/useStoriesList.tsx
@@ -1,31 +1,39 @@
 import { BASE_URL } from '@/lib/fetcher';
-import { ListingsResponseType, ListingsResponseChildrenType, ListingResposeRootType } from '@/types/listings';
+import { ListingResposeRootType } from '@/types/listings';
+import { SubscriptionsResponseChildren } from '@/types/subscriptions';
 import { getCookie } from 'cookies-next';
+import React from 'react';
 import useSubscriptions from './useSubscriptions';
 
 export default function useStoriesList() {
+  const [list, setList] = React.useState<Array<SubscriptionsResponseChildren>>([]);
+  const { subscriptions } = useSubscriptions();
   const token = getCookie(`token`);
-
   const URL_PARAMS = new URLSearchParams({
     raw_json: '1',
     limit: '10',
   });
-
   const GET_PARAMS = {
     method: 'get',
     headers: { Authorization: `bearer ${token}` },
   };
 
-  const { subscriptions } = useSubscriptions();
-  const storiesList = new Set();
-
-  subscriptions.map(async (subscription) => {
-    const res = await fetch(`${BASE_URL}${subscription.data.url}?${URL_PARAMS}`, GET_PARAMS);
-    const data: ListingResposeRootType = await res.json();
-    data.data.children.map((item) => (item.data.is_video ? storiesList.add(subscription.data.url) : () => {}));
-  });
+  React.useEffect(() => {
+    (async () => {
+      const ListingsWithVideo = new Set();
+      await Promise.all(
+        subscriptions.map(async (subscription) => {
+          const res = await fetch(`${BASE_URL}${subscription.data.url}?${URL_PARAMS}`, GET_PARAMS);
+          const data: ListingResposeRootType = await res.json();
+          data.data.children.map((item) => (item.data.is_video ? ListingsWithVideo.add(subscription.data.url) : () => {}));
+        }),
+      );
+      const list = subscriptions.filter((item) => ListingsWithVideo.has(item.data.url));
+      setList(list);
+    })();
+  }, []);
 
   return {
-    storiesList: storiesList,
+    data: list,
   };
 }
