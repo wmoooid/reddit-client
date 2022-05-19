19a13cd6834c41bd8687bbfddf2c67524ad29e97
wmoooid <threeqletters@gmail.com>
2022-05-19 19:18:10 +0400

    Add key to `StoriesBarPlaceholder`

--- components/placeholders/StoriesBar.placeholder.tsx
+++ components/placeholders/StoriesBar.placeholder.tsx
@@ -4,8 +4,8 @@ export const StoriesBarPlaceholder = () => {
       <div className='ph_stories-bar'>
         {Array(13)
           .fill('')
-          .map(() => (
-            <div className='ph_stories-bar-item'>
+          .map((el, i) => (
+            <div key={i} className='ph_stories-bar-item'>
               <div style={{ width: '4rem', height: '4rem' }} className='ph_circle'></div>
               <div style={{ width: '4rem' }} className='ph_text'></div>
             </div>
