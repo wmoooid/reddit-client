51a89c8d170504dc1e3e8d403ada6413006d3f00
wmoooid <threeqletters@gmail.com>
2022-05-20 10:25:58 +0400

    `Stories bar` placeholder change

--- components/placeholders/StoriesBar.placeholder.tsx
+++ components/placeholders/StoriesBar.placeholder.tsx
@@ -1,15 +1,15 @@
 export const StoriesBarPlaceholder = () => {
   return (
-    <div className='placeholder'>
+    <div className='placeholder' style={{ width: '100%', height: '100%' }}>
       <div className='ph_stories-bar'>
-        {Array(13)
+        {/* {Array(13)
           .fill('')
           .map((el, i) => (
             <div key={i} className='ph_stories-bar-item'>
               <div style={{ width: '4rem', height: '4rem' }} className='ph_circle'></div>
               <div style={{ width: '4rem' }} className='ph_text'></div>
             </div>
-          ))}
+          ))} */}
       </div>
     </div>
   );
